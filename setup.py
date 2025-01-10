a17b5828a590f017d5b0b3cdffc6016470996e4d
Remy Paquette <remy.paquette@gmail.com>
2025-01-10 14:36:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
     install_requires=[
         'pygame'
     ],
