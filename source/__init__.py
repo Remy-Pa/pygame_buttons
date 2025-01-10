84962e9893ca8ed336e3e5d43418ded62634fd14
Remy Paquette <remy.paquette@gmail.com>
2025-01-10 13:46:23 -0500

    Update __init__.py

--- source/__init__.py
+++ source/__init__.py
@@ -1,6 +1,3 @@
-import pygame as pg
-
-from .pygame_button import PygameButton
 from .pygame_pressable_button import PygamePressableButton
 from .pygame_togglable_button import PygameTogglableButton
 
