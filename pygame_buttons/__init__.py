947781f30f38d268aa88074fc89d63f50fdf5923
Remy Paquette <remy.paquette@gmail.com>
2025-01-10 15:12:57 -0500

    Update __init__.py

--- pygame_buttons/__init__.py
+++ pygame_buttons/__init__.py
@@ -1,4 +1,4 @@
-from .pygame_pressable_button import PygamePressableButton
-from .pygame_togglable_button import PygameTogglableButton
+from pygame_buttons.pygame_pressable_button import PygamePressableButton
+from pygamebuttons.pygame_togglable_button import PygameTogglableButton
 
 __all__ = ['pygame_pressable_button', 'pygame_togglable_button']
